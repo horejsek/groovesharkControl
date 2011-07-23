0ec4516b57ac4e4a7b88057b1fdc7b0ee670784d
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-23 14:38:27 +0200

    first popup

--- getData.js
+++ getData.js
@@ -2,8 +2,13 @@
 var isSomeInQueue = (document.getElementById("player_play_pause").className.indexOf('disabled') < 0);
 var isPlaying = (document.getElementById("player_play_pause").className.indexOf('pause') >= 0);
 
+var song = $("#playerDetails_nowPlaying a.song").text();
+var artist = $("#playerDetails_nowPlaying a.artist").text();
+
 chrome.extension.sendRequest({
   isSomeInQueue: isSomeInQueue,
-  isPlaying: isPlaying
+  isPlaying: isPlaying,
+  song: song,
+  artist: artist
 });
 
