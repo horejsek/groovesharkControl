f1416d4650d9ee1ea81dcff5667087082c510a74
Michal Horejsek <horejsekmichal@gmail.com>
2011-07-23 15:55:57 +0200

    now playing informations

--- getData.js
+++ getData.js
@@ -1,14 +1,23 @@
 
-var isSomeInQueue = (document.getElementById("player_play_pause").className.indexOf('disabled') < 0);
-var isPlaying = (document.getElementById("player_play_pause").className.indexOf('pause') >= 0);
+data = {
+  isSomeInQueue: !$('#player_play_pause').hasClass('disabled'),
+  isPlaying: $('#player_play_pause').hasClass('pause'),
+  nowPlaying: {}
+}
 
-var song = $("#playerDetails_nowPlaying a.song").text();
-var artist = $("#playerDetails_nowPlaying a.artist").text();
+if (data['isPlaying']) {
+  nowPlaying = {
+    song: $('#playerDetails_nowPlaying a.song').text(),
+    artist: $('#playerDetails_nowPlaying a.artist').text(),
+    album: $('#playerDetails_nowPlaying a.album').text(),
+    positionInQueue: $('#queue_list li.queue-item-active span.position').text(),
+    times: {
+      elapsed: $('#player_times #player_elapsed').text(),
+      duration: $('#player_times #player_duration').text()
+    }
+  }
+  data['nowPlaying'] = nowPlaying;
+}
 
-chrome.extension.sendRequest({
-  isSomeInQueue: isSomeInQueue,
-  isPlaying: isPlaying,
-  song: song,
-  artist: artist
-});
+chrome.extension.sendRequest(data);
 
