3d0ae073f1b09e7044bc5329093f5d92a4ff16b1
David Rodrigues <david.proweb@gmail.com>
2011-10-01 03:27:12 -0300

    - Improvement: use Classes on popup.js;

--- javascript/popup.js
+++ javascript/popup.js
@@ -2,81 +2,84 @@
 var activeQueueSongID = -1;
 var isNowOpened = true;
 
-function init () {
-    isNowOpened = true;
+var Popup = new function(){
+	// Init popup page
+	this.init = function() {
+	    isNowOpened = true;
 
-    getData(callbackIfGroovesharkIsNotOpen=createGroovesharkTab);
-    setUpProgressbar();
+	    getData(callbackIfGroovesharkIsNotOpen=createGroovesharkTab);
+	    setUpProgressbar();
 
-    // Set the last y position on playlist
-    var lastYPosition;
+	    // Set the last y position on playlist
+	    var lastYPosition;
 
-	// Start the controller
-    controlInit(function(){
-	    // Get playlist data
-		userAction('getPlaylist', null, function(songs, activeIndex, activeId){
-			// Set the active queue song id
-		    if (activeQueueSongID !== activeId
-			&&  activeIndex !== false) {
-				// If is the first open, show body
-				if (activeQueueSongID === -1) {
-					$('body').css('display', 'block');
-				}
+		// Start the controller
+	    controlInit(function(){
+		    // Get playlist data
+			userAction('getPlaylist', null, function(songs, activeIndex, activeId){
+				// Set the active queue song id
+			    if (activeQueueSongID !== activeId
+				&&  activeIndex !== false) {
+					// If is the first open, show body
+					if (activeQueueSongID === -1) {
+						$('body').css('display', 'block');
+					}
 
-				// Set active queue song id
-		        activeQueueSongID = activeId;
+					// Set active queue song id
+			        activeQueueSongID = activeId;
 
-				// Clean the playlist
-			    var playlistItems = $('.playlist').empty();
+					// Clean the playlist
+				    var playlistItems = $('.playlist').empty();
 
-				// Print songs on playlist
-			    $.each(songs, function (index, item) {
-			        playlistItems.append(
-			            $('<div class="item" />')
-			            	.toggleClass('odd', index % 2 === 0)
-			            	.toggleClass('active', item.queueSongID === activeId)
-				            .text(item.ArtistName + ' - ' + item.SongName)
-				            .click(function () {
-				                userAction("playSongInQueue", [item.queueSongID])
-				            })
-			        );
-			    });
+					// Print songs on playlist
+				    $.each(songs, function (index, item) {
+				        playlistItems.append(
+				            $('<div class="item" />')
+				            	.toggleClass('odd', index % 2 === 0)
+				            	.toggleClass('active', item.queueSongID === activeId)
+					            .text(item.ArtistName + ' - ' + item.SongName)
+					            .click(function () {
+					                userAction("playSongInQueue", [item.queueSongID])
+					            })
+				        );
+				    });
 
-				// Scroll playlist to this music
-				var playlistObject = $('.playlist');
-				var activeItem = $('.playlist .item.active');
-				var scrollToY = activeItem.prop('offsetTop') + ( activeItem.height() / 2 )
-					- ( playlistObject.height() / 2 ) - playlistObject.prop('offsetTop');
+					// Scroll playlist to this music
+					var playlistObject = $('.playlist');
+					var activeItem = $('.playlist .item.active');
+					var scrollToY = activeItem.prop('offsetTop') + ( activeItem.height() / 2 )
+						- ( playlistObject.height() / 2 ) - playlistObject.prop('offsetTop');
 
-				// Scroll Top Y need be 0 or upper
-				if (scrollToY < 0) {
-					scrollToY = 0;
-				}
+					// Scroll Top Y need be 0 or upper
+					if (scrollToY < 0) {
+						scrollToY = 0;
+					}
 
-				// If the popup is open now, will do a small animation
-				if (!lastYPosition) {
-					lastYPosition = scrollToY + ( scrollToY === 0 ? 200 : -200 );
-					playlistObject.prop('scrollTop', lastYPosition);
-				}
+					// If the popup is open now, will do a small animation
+					if (!lastYPosition) {
+						lastYPosition = scrollToY + ( scrollToY === 0 ? 200 : -200 );
+						playlistObject.prop('scrollTop', lastYPosition);
+					}
 
-				// Animate and align the active song on center of playlist if possible
-				playlistObject.animate({
-					scrollTop: scrollToY
-				}, 1000);
-		    }
-		    else
-		    // If playlist is empty, block the UI
-		    if (activeIndex === false){
-		    	goToGroovesharkTab();
-		    }
+					// Animate and align the active song on center of playlist if possible
+					playlistObject.animate({
+						scrollTop: scrollToY
+					}, 1000);
+			    }
+			    else
+			    // If playlist is empty, block the UI
+			    if (activeIndex === false){
+			    	goToGroovesharkTab();
+			    }
+			});
 		});
-	});
 
-	// Close window if tab is closed
-	onTabCloseAccept();
+		// Close window if tab is closed
+		onTabCloseAccept();
 
-    if (isNotificationOpen()) hidePin();
-    else showPin();
+	    if (isNotificationOpen()) hidePin();
+	    else showPin();
+	}
 }
 
 function showPin () {
